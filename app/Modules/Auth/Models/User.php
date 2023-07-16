3b8d06fba04a6013a6ed606a9bc62bff646f6dc8
saddam <saddam@vaultspay.ae>
2023-07-16 16:28:17 +0500

    Created User Modal

---
+++ app/Modules/Auth/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Modules\Auth\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['name','email','password'];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = ['password'];
+}
