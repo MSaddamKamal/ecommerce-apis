8c4c853836c234d96423cd1f9c01fa406e83aebe
saddam <saddam@vaultspay.ae>
2023-07-16 18:46:28 +0500

    Created Analytics Events For Item Add, Purchsased, View & User Register

---
+++ app/Modules/Analytics/Events/ItemPurchased.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Modules\Analytics\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Modules\Analytics\DTO\AnalyticsStoreDTO;
+
+class ItemPurchased
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     *
+     * @var AnalyticsStoreDTO $analyticsStoreDTO
+     */
+    public AnalyticsStoreDTO $analyticsStoreDTO;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param AnalyticsStoreDTO $analyticsStoreDTO
+     */
+    public function __construct(AnalyticsStoreDTO $analyticsStoreDTO)
+    {
+        $this->analyticsStoreDTO = $analyticsStoreDTO;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
